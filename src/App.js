3d97af399214ad8570f1e9576f1e6d95a02e48fe
EvesLastDescendant <clydewitoutabonnie@gmail.com>
2023-08-31 14:24:16 +0100

    two new directives and updated App.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
+import Home from "./routes/Home";
 import './App.css'
 
 function App () {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">
-                Read App
-            </h1>
+            <Home />
         </>
     )
 }
